97f66aff43b6431f0a8ae7f53ce15115cc4bca2e
David Przybilla <dav.alejandro@gmail.com>
2023-03-27 22:58:55 +0900

    bootstrapping

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Dumme hellow world";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 self.lastModifiedDate;
+
+      # System types to support.
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in rec {
+          go-sample = pkgs.buildGoModule {
+            pname = "go-sample";
+            inherit version;
+            src = ./.;
+            vendorSha256 =
+              "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          };
+          default = go-sample;
+        });
+
+      apps = forAllSystems (system: rec {
+        go-sample = {
+          type = "app";
+          program = "${self.packages.${system}.go-hello}/bin/go-sample";
+        };
+        default = go-sample;
+      });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.default);
+
+      defaultApp = forAllSystems (system: self.apps.${system}.default);
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls goimports go-tools ];
+          };
+        });
+
+      devShell = forAllSystems (system: self.devShells.${system}.default);
+    };
+}
---
+++ some_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitializeAndGetAdminClient(t *testing.T) {
+	assert.NoError(t, nil)
+}
